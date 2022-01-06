02052aea733d3c2a758ab12402c93251adcf65d4
charyveer75 <49182903+charyveer75@users.noreply.github.com>
2022-01-07 00:30:52 +0530

    Update EmployeeService.java

--- src/main/java/com/mt/services/EmployeeService.java
+++ src/main/java/com/mt/services/EmployeeService.java
@@ -22,9 +22,9 @@ public class EmployeeService {
 			throws JSONException {
 
 		JSONObject js = new JSONObject();
-		js.put("Name", "Mithun Technologies");
-		js.put("Calling Name", "Mithun");
-		js.put("DOB", "08-Nov-2011");
+		js.put("Name", "Madhusudhanachari");
+		js.put("Calling Name", "Madhu");
+		js.put("DOB", "09-Nov-1993");
 		js.put("Hobbies", "Reading Technical Blogs,Teaching, Helping to Poor People..");
 		js.put("Places he like", "His native place");
 
